0d364e5dbe022da69bc40bbe007a13466c437f1b
findshen <sx0523@gmail.com>
2021-05-31 14:17:45 +0800

    sdf

--- js/common.js
+++ js/common.js
@@ -1,12 +1,11 @@
 
-var apiUrl="http://129.204.3.98:8020";
-var fileServerUrl="http://129.204.3.98:8888";
+// var apiUrl="http://129.204.3.98:8020";
+// var fileServerUrl="http://129.204.3.98:8888";
 
 
 
-
-// var apiUrl="http://localhost:8020";
-// var fileServerUrl="http://localhost:8888";
+var apiUrl="http://localhost:8020";
+var fileServerUrl="http://localhost:8888";
 
 //login?
 judgeLogin();
